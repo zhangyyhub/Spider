de414343402d710579d4dc6a724de664c175970c
yannic <zyyyxdz@163.com>
2021-12-18 16:56:01 +0800

    post method submit data example.

---
+++ 1、爬虫入门/1.5_urllib_POST方法提交数据.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# filename  : 1.5_urllib_POST方法提交数据.py
+# datatime  : 2021/12/18 下午4:52
+# author    : Yannic Zhang
+# explain   : None
+
+from urllib.request import Request, urlopen
+from urllib.parse import urlencode
+
+request_obj = Request('http://httpbin.org/post')
+request_obj.add_header(
+    'User-agent',
+    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36"
+)
+
+data = urlencode({'name': '张三', 'age': '18'})
+print(data)
+
+# POST方法,From提交数据,不做url编码会有风险
+with urlopen(request_obj, data=data.encode()) as res:
+    print(res.read())
+
+
