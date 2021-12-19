44555aa8120c49a1b722695361a806e178f90b39
yannic <zyyyxdz@163.com>
2021-12-19 19:48:03 +0800

    selenium wait example.

---
+++ 4、selenium/selenium等待.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# filename  : selenium等待.py
+# datatime  : 2021/12/19 下午7:47
+# author    : Yannic Zhang
+# explain   : None
+
+"""
+强制等待：time.sleep()
+隐式等待：会在设置时间内判断是否请求完成，完成进行下一步，没完成抛出超时加载
+"""
+
+from selenium import webdriver
+
+chrome = webdriver.Chrome()
+# 隐式等待
+chrome.implicitly_wait(10)    # 最长设置20秒
+chrome.get("https://www.baidu.com/")
+
+chrome.find_element_by_id('lg')
